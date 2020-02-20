0288b143a4a8547e7f4629ed436ffbf03afebdea
YULONG HE <charlesyulong@yahoo.com>
2020-02-20 17:41:22 -0500

    updated the training table functions

--- src/postag.js
+++ src/postag.js
@@ -6,21 +6,20 @@ NLP Program to extract the postage from a training corpus and then create a post
 
 // Require needed module
 const fs = require('fs');
-const path = './corpus/WSJ_24.pos';
+const path = './corpus/WSJ_02-21.pos';
 const POStag = ['CC','CD','DT','EX','FW','IN','JJ','JJR','JJS','LS','MD','NN','NNS','NNP',
               'NNPS','PDT','POS','PRP','PP$','RB','RBR','RBS','RP','SYM','TO','UH','VB',
               'VBD','VBG','VBN','VBP','VBZ','WDT','WP','WP$','WRB','#','$','.',',',':',
-              '\"','\''];
+              '\"\"','\'\'','``','PRP$','(',')','\'','\"'];
 
 
 // In the data, TOKEN will be at index 0 and TAG will be at index 1
 const [TOKEN, TAG] = [0,1];
 
-// Initialize the prevState as a global variable that is used for record the previous state we get from the line reading.
 
 
 // Two tables of frequency
-let POStable = {}, STATEtable ={'Begin_Sent':{},'End_Sent':{}};
+let POStable = {}, STATEtable = {'Begin_Sent':{},'End_Sent':{}};
 
 // Initialize the POStable based on the POStag
 POStag.forEach(e => POStable[e] = {});
@@ -42,16 +41,16 @@ function countState(current, prevState) {
     // If prevStat is a blank line, then we increase the Begin_Sent state.
     if (prevState === undefined){
         increaseCount(STATEtable,'Begin_Sent',currentState);
-        return  currentState;
+        return currentState;
     }else {
         // If currentState reaches the line breaker(between sentences), increase count for the end state.
         if(currentState === undefined){
             increaseCount(STATEtable,'End_Sent',prevState);
-            return  undefined;
+            return undefined;
         }else {
             // If currentState and prevState both exist, then increase the count for current state to previous state.
             increaseCount(STATEtable,prevState,currentState);
-            return  currentState;
+            return currentState;
         }
 
     }
@@ -80,18 +79,15 @@ function processFile(inputFile) {
         rl = readline.createInterface(instream, outstream);
 
     rl.on('line', function (line) {
-        const data = line.split(/[\t]|[\s]+/);
 
-        // countPOS(data);
-        try {
-            prev = countState(data[TAG],prev);
-        }catch (e) {
-            console.log(e);
-        }
+        const data = line.split(/[\t\s]+/);
+        countPOS(data);
+        prev = countState(data[TAG],prev);
+
     });
 
     rl.on('close', function (line) {
-        console.log('done reading file.');
+
         fs.writeFileSync('src/POStable.json',JSON.stringify(POStable,null,' '));
         fs.writeFileSync('src/Statetable.json',JSON.stringify(STATEtable,null,' '));
 
